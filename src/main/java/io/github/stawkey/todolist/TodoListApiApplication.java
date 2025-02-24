3ad0e08dba21a4b4925da26ce213691ead9bfaaf
stawkey <82365208+stawkey@users.noreply.github.com>
2025-02-24 21:16:54 +0100

    Initial commit

---
+++ src/main/java/io/github/stawkey/todolist/TodoListApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.stawkey.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoListApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/stawkey/todolist/TodoListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.stawkey.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
